a0050e8a573daf77f00fac80c828363d044a533d
3bin-05 <ebin05reji@gmail.com>
2025-08-20 21:47:14 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/PORTFOLIO_V_2", 
+  base: "/PORTFOLIO_v_2", 
   server: {
     host: "::",
     port: 8080,
